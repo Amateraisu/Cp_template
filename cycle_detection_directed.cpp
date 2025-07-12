b60f409be109694e3cab6a3032488238280fb51f
Kai <LINKAIZXC@GMAIL.COM>
2025-07-12 22:41:58 +0800

    Create cycle_detection_directed.cpp

---
+++ cycle_detection_directed.cpp
@@ -0,0 +1,22 @@
+int n;
+vector<vector<int>> adj;
+vector<char> color;
+vector<int> parent;
+int cycle_start, cycle_end;
+
+bool dfs(int v) {
+    color[v] = 1;
+    for (int u : adj[v]) {
+        if (color[u] == 0) {
+            parent[u] = v;
+            if (dfs(u))
+                return true;
+        } else if (color[u] == 1) {
+            cycle_end = v;
+            cycle_start = u;
+            return true;
+        }
+    }
+    color[v] = 2;
+    return false;
+}
